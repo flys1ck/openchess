70f9a8235e16e61a9ce70afdce953fffd2dbd558
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-07-11 22:08:14 +0200

    change env

--- src/composables/useSupabase.ts
+++ src/composables/useSupabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { type Database } from "../database";
 
-const supabaseUrl = import.meta.env.SUPABASE_PROJECT_URL;
-const supabaseKey = import.meta.env.SUPABASE_API_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_API_URL;
+const supabaseKey = import.meta.env.SUPABASE_ANON_API_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export function useSupabase() {
