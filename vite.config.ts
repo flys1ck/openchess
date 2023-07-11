5c4d6db01f04d5e6e601878e905255bada25cbe3
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-07-11 09:07:25 +0200

    use loose paths in tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import TSConfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TSConfigPaths(),
+    TSConfigPaths({ loose: true }),
     VueRouter({
       dts: "./src/typed-router.d.ts",
     }),
