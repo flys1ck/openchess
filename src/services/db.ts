f49a614ab820922f34870ecbb4ebdfee35ebdaa6
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-10-07 01:29:15 +0200

    clean settings page

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,13 +1,10 @@
 import Database from "tauri-plugin-sql-api";
 
-const sql = (strings: TemplateStringsArray, ...expr: any[]) =>
-  strings.map((str, index) => str + (expr.length > index ? String(expr[index]) : "")).join("");
-
 const database = await Database.load("sqlite:db.sqlite");
 
 export async function getSetting(settingKey: string) {
   const setting = await database.select<{ setting_value: string }[]>(
-    sql`select setting_value from settings where setting_key = $1`,
+    `select setting_value from settings where setting_key = $1;`,
     [settingKey]
   );
 
@@ -15,8 +12,5 @@ export async function getSetting(settingKey: string) {
 }
 
 export async function setSetting(settingKey: string, settingValue: string) {
-  await database.execute(sql`update settings set setting_value = $1 where setting_key = $2;`, [
-    settingValue,
-    settingKey,
-  ]);
+  await database.execute(`update settings set setting_value = $1 where setting_key = $2;`, [settingValue, settingKey]);
 }
