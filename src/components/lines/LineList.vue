8e03f5a57a364ee410b22c2016afdcfb459f4450
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-10-18 00:02:12 +0200

    migrate line list

--- src/components/lines/LineList.vue
+++ src/components/lines/LineList.vue
@@ -14,20 +14,29 @@
 </template>
 
 <script setup lang="ts">
-import { useSupabase } from "@composables/useSupabase";
 import { AcademicCapIcon } from "@heroicons/vue/20/solid";
 import { useBreadcrumbs } from "@stores/useBreadcrumbs";
 import { useRoute } from "vue-router/auto";
 import BaseCard from "../base/BaseCard.vue";
+import { db, select, selectFirst } from "@/services/database";
 
-const supabase = useSupabase();
-// TODO: move route param to chapter
+// TODO: move route param to chapter (props)
 const route = useRoute("/studies/[studyId]/chapters/[chapterId]/");
 
-const [{ data: chapter }, { data: lines }] = await Promise.all([
-  supabase.from("chapters").select("id, name, studies ( id, name )").eq("id", route.params.chapterId).single(),
-  supabase.from("lines").select("*").eq("chapter", route.params.chapterId).order("name"),
-]);
+const chapterQuery = db
+  .selectFrom("chapters")
+  .innerJoin("studies", "studies.id", "chapters.study")
+  .select(["chapters.id", "chapters.name", "studies.id as study_id", "studies.name as study_name"])
+  .where("chapters.id", "=", Number(route.params.chapterId))
+  .compile();
+
+const lineQuery = db
+  .selectFrom("lines")
+  .selectAll()
+  .where("chapter", "=", Number(route.params.chapterId))
+  .orderBy("name")
+  .compile();
+const [chapter, lines] = await Promise.all([selectFirst(chapterQuery), select(lineQuery)]);
 
 const { setBreadcrumbs } = useBreadcrumbs();
 setBreadcrumbs([
@@ -37,12 +46,12 @@ setBreadcrumbs([
     to: "/studies/",
   },
   {
-    name: chapter?.studies?.name ?? "",
-    to: `/studies/${chapter?.studies?.id}`,
+    name: chapter.study_name,
+    to: `/studies/${chapter.study_id}`,
   },
   {
-    name: chapter?.name ?? "",
-    to: `/studies/${chapter?.studies?.id}/chapters/${chapter?.id}`,
+    name: chapter.name,
+    to: `/studies/${chapter.study_id}/chapters/${chapter.id}`,
   },
 ]);
 </script>
