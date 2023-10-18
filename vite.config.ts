e911960ad6727bd1b18d1599e25abfff9260422e
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-10-18 22:57:57 +0200

    remove supabase

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_", "SUPABASE_"],
+  envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
     target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
