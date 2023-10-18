be9834542e72e4700f8659cedb082c668f9349e4
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-10-18 22:15:49 +0200

    migrate line page

--- src/pages/studies/[studyId]/chapters/[chapterId]/lines/[lineId].vue
+++ src/pages/studies/[studyId]/chapters/[chapterId]/lines/[lineId].vue
@@ -8,10 +8,10 @@
 </template>
 
 <script setup lang="ts">
+import { db, selectFirst } from "@/services/database";
 import GameChessboard from "@components/GameChessboard.vue";
 import GameContextSidebar from "@components/sidebar/GameContextSidebar.vue";
 import { useGame } from "@composables/useGame";
-import { useSupabase } from "@composables/useSupabase";
 import { AcademicCapIcon } from "@heroicons/vue/20/solid";
 import { useBreadcrumbs } from "@stores/useBreadcrumbs";
 import { definePage, useRoute } from "vue-router/auto";
@@ -25,46 +25,49 @@ definePage({
 const { setBreadcrumbs } = useBreadcrumbs();
 
 const route = useRoute("/studies/[studyId]/chapters/[chapterId]/lines/[lineId]");
-const supabase = useSupabase();
-
 const game = useGame();
-supabase
-  .from("lines")
-  .select("name, pgn, study(id, name), chapter(id, name), is_white")
-  .eq("id", route.params.lineId)
-  .limit(1)
-  .single()
-  .then(({ data: line }) => {
-    if (!line) return;
-    if (!line.is_white) game.toggleOrientation();
-    game.tree.fromPgn(line.pgn);
-    if (!game.tree.root.value) return;
-    game.setActivePosition(game.tree.root.value);
 
-    // set breadcrumbs
-    setBreadcrumbs([
-      {
-        icon: AcademicCapIcon,
-        name: "Studies",
-        to: "/studies/",
-      },
-      {
-        //@ts-ignore https://github.com/supabase/postgrest-js/issues?q=is:issue+is:open+SelectQueryError
-        name: line?.study.name ?? "",
-        //@ts-ignore https://github.com/supabase/postgrest-js/issues?q=is:issue+is:open+SelectQueryError
-        to: `/studies/${line?.study.id}`,
-      },
-      {
-        //@ts-ignore https://github.com/supabase/postgrest-js/issues?q=is:issue+is:open+SelectQueryError
-        name: line?.chapter.name ?? "",
-        //@ts-ignore https://github.com/supabase/postgrest-js/issues?q=is:issue+is:open+SelectQueryError
-        to: `/studies/${line?.study.id}/chapters/${line?.chapter?.id}`,
-      },
-      {
-        name: line?.name ?? "",
-        //@ts-ignore https://github.com/supabase/postgrest-js/issues?q=is:issue+is:open+SelectQueryError
-        to: `/studies/${line?.study.id}/chapters/${line?.chapter?.id}/lines/${line?.id}`,
-      },
-    ]);
-  });
+const lineQuery = db
+  .selectFrom("lines")
+  .innerJoin("studies", "studies.id", "lines.study")
+  .innerJoin("chapters", "chapters.id", "lines.chapter")
+  .select([
+    "lines.id",
+    "lines.name",
+    "lines.pgn",
+    "lines.orientation",
+    "studies.id as study_id",
+    "studies.name as study_name",
+    "chapters.id as chapter_id",
+    "chapters.name as chapter_name",
+  ])
+  .where("lines.id", "=", Number(route.params.lineId))
+  .compile();
+selectFirst(lineQuery).then((line) => {
+  if (line.orientation === "black") game.toggleOrientation();
+  game.tree.fromPgn(line.pgn);
+  if (!game.tree.root.value) return;
+  game.setActivePosition(game.tree.root.value);
+
+  // set breadcrumbs
+  setBreadcrumbs([
+    {
+      icon: AcademicCapIcon,
+      name: "Studies",
+      to: "/studies/",
+    },
+    {
+      name: line.study_name,
+      to: `/studies/${line.study_id}`,
+    },
+    {
+      name: line.chapter_name,
+      to: `/studies/${line.study_id}/chapters/${line.chapter_id}`,
+    },
+    {
+      name: line.name,
+      to: `/studies/${line.study_id}/chapters/${line.chapter_id}/lines/${line.id}`,
+    },
+  ]);
+});
 </script>
