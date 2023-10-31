32a6bf60219210c4d75cfff11144629e0505be55
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-10-31 16:43:30 +0100

    fix sidebar section headings

---
+++ src/components/base/BaseSidebarSectionHeading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="relative bg-gray-50 py-2 text-center">
+    <span class="absolute inset-x-2 top-1/2 border-t" aria-hidden="true" />
+    <span class="relative bg-inherit px-2 text-xs font-light uppercase tracking-wider text-gray-500">
+      <slot />
+    </span>
+  </div>
+</template>
--- src/components/sidebar/GameContextSidebar.vue
+++ src/components/sidebar/GameContextSidebar.vue
@@ -2,25 +2,18 @@
   <TabGroup as="aside" class="flex w-96 shrink-0 flex-col border-l border-gray-200" manual>
     <TabList class="flex justify-around border-b border-gray-200">
       <Tab v-for="tab in TABS" :key="tab" as="template" v-slot="{ selected }">
-        <button
-          :class="
-            selected
-              ? 'border-orange-400 text-orange-400'
-              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-          "
-          class="w-1/2 whitespace-nowrap border-b-2 px-1 py-2 text-sm font-medium focus:outline-none"
-        >
+        <button :class="selected
+          ? 'border-orange-400 text-orange-400'
+          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+          " class="w-1/2 whitespace-nowrap border-b-2 px-1 py-2 text-sm font-medium focus:outline-none">
           {{ tab }}
         </button>
       </Tab>
     </TabList>
     <TabPanels as="template">
       <!-- Game Tab -->
-      <TabPanel
-        :unmount="false"
-        class="flex flex-grow flex-col justify-between overflow-hidden focus:outline-none"
-        :tab-index="-1"
-      >
+      <TabPanel :unmount="false" class="flex flex-grow flex-col justify-between overflow-hidden focus:outline-none"
+        :tab-index="-1">
         <!-- Engine evaluation -->
         <Suspense>
           <GameEvaluation :fen="game.fen" />
@@ -29,47 +22,24 @@
         <template v-if="game.tree.root.value">
           <div class="flex-grow overflow-y-auto">
             <div class="grid grid-cols-16 items-stretch">
-              <GameTreeItem
-                :node="game.tree.root.value"
-                :active-node-id="game.tree.activeNode.value?.id"
-                @nodeselect="game.setActivePosition"
-              />
+              <GameTreeItem :node="game.tree.root.value" :active-node-id="game.tree.activeNode.value?.id"
+                @nodeselect="game.setActivePosition" />
             </div>
           </div>
           <div class="flex flex-col gap-2 border-t p-4">
             <div class="flex justify-center gap-4">
-              <BaseButton
-                variant="secondary"
-                size="sm"
-                :prefix-icon="ChevronDoubleLeftIcon"
-                :disabled="game.tree.root.value?.id === game.tree.activeNode.value?.id"
-                aria-label="Skip to first move"
-                @click="game.tree.toFirstMove"
-              />
-              <BaseButton
-                variant="secondary"
-                size="sm"
-                :prefix-icon="ChevronLeftIcon"
+              <BaseButton variant="secondary" size="sm" :prefix-icon="ChevronDoubleLeftIcon"
+                :disabled="game.tree.root.value?.id === game.tree.activeNode.value?.id" aria-label="Skip to first move"
+                @click="game.tree.toFirstMove" />
+              <BaseButton variant="secondary" size="sm" :prefix-icon="ChevronLeftIcon"
                 :disabled="!game.tree.activeNode.value || !game.tree.activeNode.value.previousPosition"
-                aria-label="Previous move"
-                @click="game.tree.toPreviousMove"
-              />
-              <BaseButton
-                variant="secondary"
-                size="sm"
-                :prefix-icon="ChevronRightIcon"
+                aria-label="Previous move" @click="game.tree.toPreviousMove" />
+              <BaseButton variant="secondary" size="sm" :prefix-icon="ChevronRightIcon"
+                :disabled="!game.tree.activeNode.value || !game.tree.activeNode.value.nextPosition" aria-label="Next move"
+                @click="game.tree.toNextMove" />
+              <BaseButton variant="secondary" size="sm" :prefix-icon="ChevronDoubleRightIcon"
                 :disabled="!game.tree.activeNode.value || !game.tree.activeNode.value.nextPosition"
-                aria-label="Next move"
-                @click="game.tree.toNextMove"
-              />
-              <BaseButton
-                variant="secondary"
-                size="sm"
-                :prefix-icon="ChevronDoubleRightIcon"
-                :disabled="!game.tree.activeNode.value || !game.tree.activeNode.value.nextPosition"
-                aria-label="Skip to last move"
-                @click="game.tree.toLastMove"
-              />
+                aria-label="Skip to last move" @click="game.tree.toLastMove" />
             </div>
           </div>
         </template>
@@ -80,11 +50,13 @@
       </TabPanel>
       <!-- Settings Tab -->
       <TabPanel>
-        <div class="flex flex-col p-4">
-          <BaseSectionHeading heading="Game & Board" />
+        <BaseSidebarSectionHeading>Game & Board</BaseSidebarSectionHeading>
+        <div class="p-4 flex flex-col">
           <BaseButton variant="secondary" @click="game.createNewGame">New Game</BaseButton>
           <BaseButton class="mt-2" variant="secondary" @click="game.toggleOrientation">Toggle Orientation</BaseButton>
-          <BaseSectionHeading class="mt-2" heading="Engine" />
+        </div>
+        <BaseSidebarSectionHeading class="mt-2">Engine</BaseSidebarSectionHeading>
+        <div class="p-4">
           <div class="flex items-center justify-between">
             <BaseInputLabel html-for="depth">Multiple Lines</BaseInputLabel>
             <span class="text-sm text-gray-600">{{ settings.engineLines[0] }}</span>
@@ -103,10 +75,9 @@
 
 <script setup lang="ts">
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/vue";
-import BaseButton from "../base/BaseButton.vue";
-import GameTreeItem from "../sidebar/GameTreeItem.vue";
-import GamePositions from "../sidebar/GamePositions.vue";
-
+import BaseButton from "@components/base/BaseButton.vue";
+import GameTreeItem from "@components/sidebar/GameTreeItem.vue";
+import GamePositions from "@components/sidebar/GamePositions.vue";
 import GameEvaluation from "@components/sidebar/GameEvaluation.vue";
 import { useGame } from "@composables/useGame";
 import {
@@ -118,7 +89,7 @@ import {
 import { onKeyStroke } from "@vueuse/core";
 import BaseSlider from "@/components/base/BaseSlider.vue";
 import BaseInputLabel from "@/components/base/BaseInputLabel.vue";
-import BaseSectionHeading from "@/components/base/BaseSectionHeading.vue";
+import BaseSidebarSectionHeading from "@/components/base/BaseSidebarSectionHeading.vue";
 import { useSettings } from "@/stores/useSettings";
 
 const props = defineProps<{
