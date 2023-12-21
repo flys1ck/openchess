ef36cc9a61d9257aaad9e556ab9415c7e1137d1e
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-12-21 15:09:42 +0100

    show app version in sidebar

--- src/components/NavigationSidebar.vue
+++ src/components/NavigationSidebar.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="flex flex-shrink-0 flex-col bg-gray-800 py-5">
+  <div class="flex flex-shrink-0 flex-col bg-gray-800 pt-5">
     <!-- Navigation -->
     <div class="mt-5 flex flex-grow flex-col justify-between">
       <nav class="flex flex-1 flex-col gap-1">
         <RouterLink v-for="{ icon, text, to } in navigationItems" :to="to" :key="to">
           <div
             class="transition-color ml-1.5 flex flex-col items-center gap-2 overflow-hidden rounded-l-lg p-1.5 font-medium text-gray-50 hover:bg-gray-700"
-            :class="{ 'bg-gray-700': route.path.startsWith(to) }"
-          >
+            :class="{ 'bg-gray-700': route.path.startsWith(to) }">
             <Component :is="icon" class="h-6 w-6" :class="{ 'text-orange-400': route.path.startsWith(to) }" />
             <span class="text-xs lowercase">{{ text }}</span>
           </div>
         </RouterLink>
       </nav>
+      <span class="text-[0.5rem] text-gray-600 text-center">v{{ appVersion }}</span>
     </div>
   </div>
 </template>
@@ -20,6 +20,13 @@
 <script setup lang="ts">
 import { navigationItems } from "@data/navigationItems";
 import { useRoute } from "vue-router/auto";
+import { getVersion } from '@tauri-apps/api/app';
+import { onMounted, ref } from "vue";
 
 const route = useRoute();
+
+const appVersion = ref("");
+onMounted(async () => {
+  appVersion.value = await getVersion();
+});
 </script>
