29e5e2537deebe11fd9417c30a70a424f040cace
flys1ck <34650380+flys1ck@users.noreply.github.com>
2023-12-22 21:21:56 +0100

    use common path for db

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,7 @@ use std::path;
 
 fn main() {
     let app_data_path =
-        path::Path::new(&tauri::api::path::local_data_dir().unwrap()).join("com.openchess.dev");
+        path::Path::new(&tauri::api::path::data_dir().unwrap()).join("com.openchess.dev");
     let database_path = app_data_path.join("db.sqlite");
     assert!(
         app_data_path.exists() || std::fs::create_dir(&app_data_path).is_ok(),
@@ -23,7 +23,9 @@ fn main() {
         .unwrap_or_else(|| panic!("Error convert path {:?} to url", database_path));
     let mut connection = SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", &database_url));
-    connection.run_pending_migrations(MIGRATIONS).unwrap();
+    connection
+        .run_pending_migrations(MIGRATIONS)
+        .unwrap_or_else(|_| panic!("Migrations could not be ran"));
 
     tauri::Builder::default()
         .plugin(tauri_plugin_sql::Builder::default().build())
