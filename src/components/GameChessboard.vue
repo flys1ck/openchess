781cc0fb7da8a6b1e360b32c623e95453c8f874f
Gregor Zweig <zwickzwackzwieback@gmail.com>
2023-12-23 14:17:13 +0100

    size board to parent container

--- src/components/GameChessboard.vue
+++ src/components/GameChessboard.vue
@@ -1,30 +1,20 @@
 <template>
   <div class="relative">
-    <div ref="chessboardRef" class="aspect-square max-h-full"></div>
+    <div ref="chessboardRef" class="aspect-square"></div>
     <!-- promotion overlay -->
-    <div
-      v-if="game.isPromoting.value"
-      :style="`width: ${overlayWidth}px`"
-      @click="game.cancelPromotion"
-      class="absolute left-0 top-0 z-10 aspect-square bg-black/70"
-    >
+    <div v-if="game.isPromoting.value" :style="`width: ${overlayWidth}px`" @click="game.cancelPromotion"
+      class="absolute left-0 top-0 z-10 aspect-square bg-black/70">
       <div class="absolute flex h-1/2 w-1/8 items-center justify-center" :style="game.promotionStyles.value">
-        <button
-          v-for="piece in ['queen', 'rook', 'bishop', 'knight']"
-          :key="piece"
+        <button v-for="piece in ['queen', 'rook', 'bishop', 'knight']" :key="piece"
           class="aspect-square w-full scale-90 rounded bg-slate-300 hover:bg-orange-300"
-          @click.stop="game.promote(piece as PromotionPiece)"
-        >
+          @click.stop="game.promote(piece as PromotionPiece)">
           <div :class="`${game.promotionColor.value} ${piece} h-full w-full  bg-cover`" />
         </button>
       </div>
     </div>
     <!-- e2e test helper -->
-    <div
-      v-if="mode === 'development'"
-      :style="`width: ${overlayWidth}px`"
-      class="pointer-events-none absolute left-0 top-0 grid aspect-square grid-cols-8"
-    >
+    <div v-if="mode === 'development'" :style="`width: ${overlayWidth}px`"
+      class="pointer-events-none absolute left-0 top-0 grid aspect-square grid-cols-8">
       <template v-for="rank in ['1', '2', '3', '4', '5', '6', '7', '8'].reverse()" :key="rank">
         <template v-for="file in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']" :key="file">
           <span :data-test="`square-${file}${rank}`"></span>
